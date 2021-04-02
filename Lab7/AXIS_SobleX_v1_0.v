95bc5f377180926af8187ec7d546d42838af5041
Fuwei Huang <67210826+BakingSodaSpirit@users.noreply.github.com>
2021-04-01 22:50:13 -0600

    Stream file/Test bench

---
+++ Lab7/AXIS_SobleX_v1_0.v
@@ -0,0 +1,80 @@
+
+`timescale 1 ns / 1 ps
+
+module AXIS_SobleX_v1_0 #
+	(
+		// Users to add parameters here
+
+		// User parameters ends
+		// Do not modify the parameters beyond this line
+
+
+		// Parameters of Axi Slave Bus Interface S00_AXIS
+		parameter integer C_S00_AXIS_TDATA_WIDTH	= 32,
+
+		// Parameters of Axi Master Bus Interface M00_AXIS
+		parameter integer C_M00_AXIS_TDATA_WIDTH	= 32,
+		parameter integer C_M00_AXIS_START_COUNT	= 32
+	)
+	(
+		// Users to add ports here
+
+		// User ports ends
+		// Do not modify the ports beyond this line
+
+
+		// Ports of Axi Slave Bus Interface S00_AXIS
+		input wire  s00_axis_aclk,
+		input wire  s00_axis_aresetn,
+		output wire  s00_axis_tready,
+		input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
+		input wire  s00_axis_tlast,
+		input wire  s00_axis_tvalid,
+
+		// Ports of Axi Master Bus Interface M00_AXIS
+		input wire  m00_axis_aclk,
+		input wire  m00_axis_aresetn,
+		output wire  m00_axis_tvalid,
+		output wire [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata,
+		output wire  m00_axis_tlast,
+		input wire  m00_axis_tready
+	);
+	
+	reg m00_axis_tvalid_reg;
+	reg m00_axis_tlast_reg;
+	reg [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata_reg;
+	
+    reg s00_axis_tready_reg;
+    
+    wire [C_M00_AXIS_TDATA_WIDTH-1 : 0] data_out;
+    
+    
+    assign m00_axis_tvalid = m00_axis_tvalid_reg;
+    assign m00_axis_tlast = m00_axis_tlast_reg;
+    assign m00_axis_tdata = m00_axis_tdata_reg;
+    assign s00_axis_tready = s00_axis_tready_reg;
+   
+    
+    SobleX fir(m00_axis_aclk,m00_axis_tvalid, s00_axis_tdata, data_out);
+    
+    always @(posedge s00_axis_aclk)begin
+        if (s00_axis_aresetn==0)begin
+            s00_axis_tready_reg <= 0;
+        end else begin
+            s00_axis_tready_reg <= m00_axis_tready;
+        end
+    end
+	
+	always @(posedge m00_axis_aclk)begin
+        if (m00_axis_aresetn==0)begin
+            m00_axis_tvalid_reg <= 0;
+            m00_axis_tlast_reg <= 0;
+            m00_axis_tdata_reg <= 0;
+        end else begin
+            m00_axis_tvalid_reg <= s00_axis_tvalid;
+            m00_axis_tlast_reg <= s00_axis_tlast;
+            m00_axis_tdata_reg <=data_out;
+        end
+    end
+
+endmodule
